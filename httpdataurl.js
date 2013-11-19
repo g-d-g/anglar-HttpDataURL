5b6c92d746015510efc165076560c7810365918c
Jimmy Wärting <jimmy@warting.se>
2013-11-19 20:41:39 +0100

    no message

---
+++ httpdataurl.js
@@ -0,0 +1,25 @@
+app.factory('HttpDataURL', function($q) {
+	function decode(arraybuffer, responseHeader) {
+		
+		var binary = '';
+		var bytes = new Uint8Array( arraybuffer )
+		var len = bytes.byteLength;
+		for (var i = 0; i < len; i++) {
+			binary += String.fromCharCode( bytes[ i ] )
+		}
+		
+		return "data:"+responseHeader("content-type")+";base64,"+window.btoa( binary );
+	}
+
+	return {
+		// optional method
+		request: function(config) {
+			if(config.responseType == "dataURL"){
+				config.responseType = "arraybuffer";
+				config.transformResponse.push(decode);
+			}
+			// do something on success
+			return config || $q.when(config);
+		}
+	}
+});
